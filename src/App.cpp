28111d3ed4560c33d779488af3f4e0086abfce6c
coltginger <gnbarroso@gmail.com>
2024-03-27 20:03:12 +0000

    Menu

---
+++ src/App.cpp
@@ -0,0 +1,35 @@
+#include "App.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+void App::run(){
+    bool STOP = false;
+
+    while(!STOP){
+        cout << "1. Max amount of water that can reach each or a specific city. " << endl
+             << "0. Close the program."
+             << "Please select an option (number): ";
+
+        int option;
+        int a, id;
+        cin >> option;
+
+        switch (option) {
+            case 0:
+                STOP = true;
+                break;
+
+            case 1:
+                cout << endl << "1. Each city." << endl
+                     << "2. Specific city" << endl;
+                cin >> a;
+                if(a == 2){
+                    cout << endl << "City ID: " << endl;
+                    cin >> id;
+                }
+                break;
+
+        }
+    }
+};
---
+++ src/App.h
@@ -0,0 +1,17 @@
+#include "GraphManager.h"
+
+#ifndef DA_PROJ_1_APP_H
+#define DA_PROJ_1_APP_H
+
+
+class App {
+private:
+    GraphManager _graphManager;
+
+public:
+    App();
+    void run();
+};
+
+
+#endif //DA_PROJ_1_APP_H
